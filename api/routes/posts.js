359cb6f2618ba4bc130298de3b4316200bd123f4
moni97 <monisam97@gmail.com>
2023-01-17 22:58:43 -0600

    api code

---
+++ api/routes/posts.js
@@ -0,0 +1,85 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const Post = require("../models/Post");
+
+//Create
+router.post("/", async(req, res) => {
+    const newPost = new Post(req.body);
+    try {
+        const savedPost = await newPost.save()
+        res.status(200).json(savedPost);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+})
+
+//Update 
+router.put("/:id", async(req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.username === req.body.username) {
+            try {
+                const updatedPost = await Post.findByIdAndUpdate(
+                    req.params.id, 
+                    { $set: req.body}, 
+                    {new: true}
+                );
+                res.status(200).json(updatedPost);
+            } catch(err) {
+                res.status(500).json(err)
+            }
+        }
+    } catch(err) {
+        res.status(500).json("You can update only from your account")
+    }
+})
+
+//Delete
+router.delete("/:id", async(req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.username === req.body.username) {
+            try {
+                await post.delete();
+                res.status(200).json("Post deleted");
+            } catch(err) {
+                res.status(500).json(err)
+            }
+        }
+    } catch(err) {
+        res.status(500).json("You can update only from your account")
+    }
+})
+
+//Get 
+router.get("/:id", async(req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post)
+    } catch(err) {
+        res.status(400).json("Unable to get the post")
+    }
+})
+
+//Get all posts
+router.get("/", async(req, res) => {
+    const username = req.query.user;
+    const catName = req.query.cat;
+    try {
+        let posts;
+        if (username) {
+            posts = await Post.find({username})
+        } else if(catName) {
+            posts = await Post.find({categories: {
+                $in: [catName]
+            }})
+        } else {
+            posts = await Post.find();
+        }
+        res.status(200).json(posts)
+    } catch(err) {
+        res.status(400).json("Unable to get the post")
+    }
+})
+
+module.exports = router
